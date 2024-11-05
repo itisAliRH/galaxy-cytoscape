5b0c7f66b3d49cab6b4af26f2f3d65d14aa0d9d1
Alireza Heidari <itisalirh@gmail.com>
2024-11-05 17:07:21 +0100

    init cytoscope plugin in Plugin.vue

--- src/Plugin.vue
+++ src/Plugin.vue
@@ -1,19 +1,76 @@
-<script setup>
-import { onMounted, ref, watch } from "vue";
+<script setup lang="ts">
+import axios from "axios";
+import Cytoscape from "cytoscape";
+import { onMounted, onUnmounted, ref, watch } from "vue";
 
-const props = defineProps({
-    datasetId: String,
-    datasetUrl: String,
-    root: String,
-    settings: Object,
-    specs: Object,
-    tracks: Array,
-});
+import { parse_sif, runSearchAlgorithm, runTraversalType, styleGenerator } from "./utils";
+
+interface Props {
+    datasetId: string;
+    datasetUrl: string;
+    root: string;
+    settings: object;
+    specs?: object;
+    tracks: unknown[];
+}
+
+const props = defineProps<Props>();
 
 const viewport = ref(null);
 
+const dataset = ref(null);
+const cytoscape = ref(null);
+
 async function render() {
-    /** Place your render function here! */
+    await getDataset();
+
+    cytoscape.value = Cytoscape({
+        container: viewport.value,
+        elements: dataset.value,
+        layout: {
+            name: props.settings.layout_name,
+            idealEdgeLength: 100,
+            nodeOverlap: 20,
+        },
+        minZoom: 0.1,
+        maxZoom: 20,
+        style: styleGenerator(props.settings),
+    });
+
+    if (props.settings.search_algorithm === "kruskal") {
+        cytoscape.value.elements().kruskal().edges().addClass("searchpath");
+    }
+
+    cytoscape.value.on("tap", "node", (e) => {
+        const ele = e.target;
+
+        const search_algorithm = props.settings.search_algorithm;
+        const traversal_type = props.settings.graph_traversal;
+
+        if (search_algorithm) {
+            runSearchAlgorithm(cytoscape.value, ele.id(), search_algorithm);
+        } else if (traversal_type) {
+            runTraversalType(cytoscape.value, ele.id(), traversal_type);
+        }
+    });
+
+    window.addEventListener("resize", function () {
+        cytoscape.value
+            .layout({
+                name: props.settings.layout_name,
+            })
+            .run();
+    });
+}
+
+async function getDataset() {
+    const { data } = await axios.get(props.datasetUrl);
+
+    if (dataset.file_ext === "sif") {
+        dataset.value = parse_sif(data).content;
+    } else {
+        dataset.value = data.elements ? data.elements : data;
+    }
 }
 
 onMounted(() => {
@@ -25,28 +82,16 @@ watch(
     () => render(),
     { deep: true },
 );
+
+onUnmounted(() => {
+    window.removeEventListener("resize", function () {
+        console.log("Removing resize event listener");
+    });
+
+    cytoscape.value.destroy();
+});
 </script>
 
 <template>
-    <div class="flex items-center justify-center min-h-screen bg-gray-900 text-white" ref="viewport">
-        <div class="text-center p-6 max-w-lg bg-gray-800 rounded-lg shadow-lg">
-            <h1 class="text-2xl font-bold mb-4">Welcome to the Galaxy Charts Starter!</h1>
-            <p class="text-lg mb-6">Initialization Complete!</p>
-            <p class="mb-6">
-                Begin creating your visualization in <code class="bg-gray-700 p-1 rounded">src/Plugin.vue</code>.
-            </p>
-            <p class="mb-6">
-                Configure your XML in <code class="bg-gray-700 p-1 rounded">public/MY_VISUALIZATION.xml</code>.
-            </p>
-            <p class="mb-6">
-                Provide local runtime settings in <code class="bg-gray-700 p-1 rounded">src/main.js</code>.
-            </p>
-            <a
-                href="https://galaxyproject.github.io/galaxy-charts/"
-                target="_blank"
-                class="text-blue-400 hover:text-blue-300 underline">
-                Galaxy Charts Documentation
-            </a>
-        </div>
-    </div>
+    <div class="min-h-screen" ref="viewport"></div>
 </template>
