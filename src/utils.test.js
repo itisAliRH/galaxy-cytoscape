6b753efbaca8e0e1ceffcf6daaffe92b2396ff69
Alireza Heidari <itisalirh@gmail.com>
2024-11-06 12:51:35 +0100

    Add unit test for runSearchAlgorithm

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,4 +1,13 @@
-import { parseSIF } from "./utils";
+import { afterEach, beforeEach, vi } from "vitest";
+import { parseSIF, runSearchAlgorithm } from "./utils";
+
+beforeEach(() => {
+    vi.useFakeTimers();
+});
+
+afterEach(() => {
+    vi.restoreAllMocks();
+});
 
 test("parseSIF", () => {
     const text = "A\tppi\tB\nB\tppi\tC\nC\tppi\tD\n";
@@ -18,3 +27,29 @@ test("parseSIF", () => {
     expect(parsed).toEqual(expected);
 });
 
+test("runSearchAlgorithm", () => {
+    const cytoscape = {
+        elements: vi.fn().mockReturnValue({
+            bfs: vi.fn().mockReturnValue({
+                path: [{ addClass: vi.fn() }, { addClass: vi.fn() }, { addClass: vi.fn() }],
+            }),
+            dfs: vi.fn().mockReturnValue({
+                path: [{ addClass: vi.fn() }, { addClass: vi.fn() }, { addClass: vi.fn() }],
+            }),
+            aStar: vi.fn().mockReturnValue({
+                path: [{ addClass: vi.fn() }, { addClass: vi.fn() }, { addClass: vi.fn() }],
+            }),
+        }),
+    };
+    const rootId = "A";
+    const type = "bfs";
+    const self = { cytoscape: cytoscape };
+    runSearchAlgorithm(cytoscape, rootId, type, self);
+
+    vi.runAllTimers();
+
+    const path = cytoscape.elements().bfs().path;
+    path.forEach((element) => {
+        expect(element.addClass).toHaveBeenCalledWith("searchpath");
+    });
+});
