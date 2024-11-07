ab6bf283b9f4e9edc917c5a876c591efd53f9eb2
guerler <aysam.guerler@gmail.com>
2024-11-07 22:09:14 +0300

    Fix attachment in production

--- src/main.js
+++ src/main.js
@@ -5,19 +5,21 @@ import "./style.css";
 import { parseXML } from "galaxy-charts-xml-parser";
 
 async function main() {
-    // Build the incoming data object
-    const dataIncoming = {
-        visualization_config: {
-            dataset_url: "MY_DATASET_ID",
-            dataset_id: "MY_DATASET_URL",
-            settings: {},
-        },
-        visualization_plugin: await parseXML("MY_VISUALIZATION.xml"),
-    };
+    if (import.meta.env.DEV) {
+        // Build the incoming data object
+        const dataIncoming = {
+            visualization_config: {
+                dataset_url: "MY_DATASET_ID",
+                dataset_id: "MY_DATASET_URL",
+                settings: {},
+            },
+            visualization_plugin: await parseXML("MY_VISUALIZATION.xml"),
+        };
 
-    // Attach config to the data-incoming attribute
-    const appElement = document.querySelector("#app");
-    appElement.setAttribute("data-incoming", JSON.stringify(dataIncoming));
+        // Attach config to the data-incoming attribute
+        const appElement = document.querySelector("#app");
+        appElement.setAttribute("data-incoming", JSON.stringify(dataIncoming));
+    }
 
     // Mount the app
     createApp({
