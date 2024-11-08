c9e7345821ffe275e0ed3d47c06ff86999571805
guerler <aysam.guerler@gmail.com>
2024-11-08 11:30:15 +0300

    Update version, component now has fallback prop named incoming

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
-import { GalaxyCharts } from "galaxy-charts";
+import { GalaxyCharts, type PluginIncomingType } from "galaxy-charts";
 import Plugin from "./Plugin.vue";
 
 defineProps<{
     credentials?: RequestCredentials;
+    incoming?: PluginIncomingType;
 }>();
 </script>
 
 <template>
-    <GalaxyCharts :credentials="credentials">
+    <GalaxyCharts :credentials="credentials" :incoming="incoming">
         <template #default="{ datasetId, datasetUrl, root, settings, specs, tracks }">
             <Plugin
                 :dataset-id="datasetId"
